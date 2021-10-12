9942af07debf9c5702558b288b9ded093c3ebdfa
Faro-Enuff <f.roth2503@gmail.com>
2021-10-12 10:27:28 +0200

    Home Componenet Style & General App Style

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -11,12 +11,18 @@ import stories from "../Components/Timeline Creative TIM/stories";
 import Enso from "../Images/Enso.png";
 
 // MUI Core Imports
-import { Card, Typography, Box } from "@mui/material";
+import { Card, Typography, Box, Container, Grid } from "@mui/material";
 import Profile from "../Components/Profile";
 import AssEqCard from "../Components/AssEqCard";
 import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  timeline: {
+    maxHeight: "68%",
+    overflow: "scroll",
+    overflowX: "hidden",
+  },
+}));
 
 const Home = () => {
   const classes = useStyles();
@@ -29,30 +35,51 @@ const Home = () => {
   console.log(userHobby);
 
   return (
-    <Box sx={{ m: "5%" }} className={classes.home}>
-      <Navbar />
-      <Box
-        sx={{ display: "flex", flexDirection: "row", width: "100%", mt: "10%" }}
-      >
-        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-          <Typography variant="h5">{`My ${userHobby?.hobby} cV`}</Typography>
+    <Container component="main" maxWidth="xs">
+      <div className={classes.home}>
+        <Box sx={{ m: "5%", overflow: "hidden" }}>
+          <Navbar />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              mt: "10%",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+              <Typography variant="h5">{`My ${userHobby?.hobby} cV`}</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                mr: "5%",
+                overflow: "hidden",
+              }}
+              className={classes.profile}
+            >
+              <Profile />
+            </Box>
+          </Box>
+          <Box
+            sx={{ display: "flex", flexDirection: "row", overflow: "hidden" }}
+          >
+            <Box>
+              <h1>Box I</h1>
+              <AssEqCard header={"Equipment"} body={userHobby?.equipment} />
+              <AssEqCard header={"Assignment"} />
+            </Box>
+            <Box sx={{ flexGrow: 1 }}>
+              <h1>Box II</h1>
+              <div className={classes.timeline}>
+                <Timeline stories={stories} />
+              </div>
+            </Box>
+          </Box>
         </Box>
-        <Box sx={{ display: "flex", alignItems: "center", mr: "5%" }}>
-          <Profile />
-        </Box>
-      </Box>
-      <Box sx={{ display: "flex", flexDirection: "row" }}>
-        <Box>
-          <h1>Box I</h1>
-          <AssEqCard header={"Equipment"} body={userHobby?.equipment} />
-          <AssEqCard header={"Assignment"} />
-        </Box>
-        <Box sx={{ flexGrow: 1 }}>
-          <h1>Box II</h1>
-          <Timeline stories={stories} />
-        </Box>
-      </Box>
-    </Box>
+      </div>
+    </Container>
   );
 };
 
