6bd43f755b947f7655b1ecf845bae6daa78825e5
Faro-Enuff <f.roth2503@gmail.com>
2021-10-20 20:54:54 +0200

    Server Cleaning

--- server/server.js
+++ server/server.js
@@ -1,6 +1,28 @@
+// Import Express into our application (express + app)
+import express from "express";
+
 // Import Passport Middleware
 import passport from "passport";
 import { jwtStrategy, googleStrategy } from "./Middleware/PassportConfig.js";
+
+// Import .env
+import dotenv from "dotenv";
+dotenv.config();
+
+// Import mongoose in our application
+import mongoose from "mongoose";
+
+// Middleware Imports
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+// Import Back-End Routes
+import userRoutes from "./routes/userRoute.js";
+import summonRoutes from "./routes/summonRoute.js";
+import hobbyRoutes from "./routes/hobbyRoute.js";
+import creationRoutes from "./routes/creationRoute.js";
+
 ////////////////////////
 ////////////////////////
 
@@ -9,18 +31,8 @@ import { jwtStrategy, googleStrategy } from "./Middleware/PassportConfig.js";
 ////////////////////////
 ////////////////////////
 
-// Import Express into our application (express + app)
-import express from "express";
-
 // Server Creation via express
 const app = express();
-
-// Import .env
-import dotenv from "dotenv";
-dotenv.config();
-
-// Import mongoose in our application
-import mongoose from "mongoose";
 
 export const connection = mongoose
   .connect(process.env.DB)
@@ -30,34 +42,29 @@ export const connection = mongoose
 ////////////////////////
 ////////////////////////
 
-// Middleware
+// !!! Middleware !!!
 
 ////////////////////////
 ////////////////////////
-
-import cookieParser from "cookie-parser";
 
 app.use(cookieParser(process.env.SECRET));
 
 // BodyParser
 app.use(express.json());
-
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
-// Cors Import + Initialization
-import cors from "cors";
+// Cors Initialization
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-import session from "express-session";
-
+// Session Initialization
 app.use(
   session({
     secret: "Our little secret.",
@@ -66,10 +73,10 @@ app.use(
   })
 );
 
-// Define Strategy
+// Initialize Passport Strategies
 passport.use("jwt", jwtStrategy);
 passport.use("google", googleStrategy);
-
+// Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -80,37 +87,6 @@ app.use(passport.session());
 
 ////////////////////////
 ////////////////////////
-
-// Import Back-End Routes
-import userRoutes from "./routes/userRoute.js";
-import summonRoutes from "./routes/summonRoute.js";
-import hobbyRoutes from "./routes/hobbyRoute.js";
-import creationRoutes from "./routes/creationRoute.js";
-
-// const isLoggedIn = (req, res, next) => {
-//   req.user ? next() : res.sendStatus(401);
-// };
-
-// app.get(
-//   "/google",
-//   passport.authenticate("google", { scope: ["email", "profile"] })
-// );
-
-// app.get(
-//   "/google/callback",
-//   passport.authenticate("google", {
-//     successRedirect: "/google-profile",
-//     failureRedirect: "/login",
-//   })
-// );
-
-// app.get("/login", (req, res) => {
-//   res.send("Something went wrong..");
-// });
-
-// app.get("/google-profile", isLoggedIn, (req, res) => {
-//   res.send("Hey");
-// });
 
 // Back-End Route to users collection
 
@@ -128,18 +104,20 @@ app.use("/api/hobbies", hobbyRoutes);
 
 app.use("/api/creations", creationRoutes);
 
-// Provide Static files
+////////////////////////////////////////////////////////////////
+// !!! Provide Static files - Divide into different Local Storages !!!
+////////////////////////////////////////////////////////////////
 
+// Local Storage for Profile Images
 app.use("/profileImage", express.static("UploadProfileImages"));
-
+// Local Storage for Summon Files
 app.use("/summonFile", express.static("UploadSummonFiles"));
 
 ////////////////////////
 ////////////////////////
 
 // !!! Connecting Server and Database !!!
-
-// Connecting to database (mongoDB Atlas "interestCV")
+// => => =>  Connecting to database (mongoDB Atlas "interestCV")
 
 ////////////////////////
 ////////////////////////
@@ -148,6 +126,7 @@ app.use("/summonFile", express.static("UploadSummonFiles"));
 const port = process.env.PORT || 5000;
 
 // TODO eventually put this listen function into the connect function of mongoDB, makes more sense to wait until the connection is set (async function for mongoose connect. Netninja min 13 of mongoDB video)
+
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
